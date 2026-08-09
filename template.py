0fdcf9ebdec682cedb9dca49b93a4a8aea82e759
thobela <thobela.sixpence@gmail.com>
2026-08-09 19:06:11 +0200

    chore: adding tests folder

--- template.py
+++ template.py
@@ -30,6 +30,7 @@ list_of_files = [
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
     f"{project_name}/configs/.gitkeep",
+    "tests/__init__.py",
     "README.md",
     "LICENSE",
     ".gitignore",
